75af6dbe84e007f4961c13b1d502d1da248d83c5
Alana <109856188+alanabueno@users.noreply.github.com>
2022-07-23 13:14:44 -0300

    pt

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Embarques de Uber em NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -16,21 +16,21 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data_load_state = st.text('Loading data...')
+data_load_state = st.text('Carregando dados...')
 data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text("Pronto! (using st.cache)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
+if st.checkbox('Mostrar Dados Brutos'):
+    st.subheader('Dados Brutos')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
+st.subheader('Número de embarques por hora')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
+hour_to_filter = st.slider('hora', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.subheader('Mapa de todos os embarques as %s:00' % hour_to_filter)
 st.map(filtered_data)
